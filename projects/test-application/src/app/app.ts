41cb6c06a19ceb89b7bde7fc7d8fe19a83efb2ee
Marcos Souza <ms@inicie.digital>
2025-09-15 10:51:45 -0300

    refactor: create correct structure

--- projects/test-application/src/app/app.ts
+++ projects/test-application/src/app/app.ts
@@ -1,10 +1,10 @@
-import { LibLinearStepperNg } from './../../../lib-linear-stepper-ng/src/lib/lib-linear-stepper-ng';
+import { StepperComponent } from './../../../lib-linear-stepper-ng/src/lib/components/stepper/stepper.component';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LibLinearStepperNg],
+  imports: [RouterOutlet, StepperComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
