299591f051c687c5ddde2482ca8227d850f98b96
Marcos Souza <ms@inicie.digital>
2025-09-16 10:03:15 -0300

    feat: create correct control dropdown

--- projects/test-application/src/app/app.ts
+++ projects/test-application/src/app/app.ts
@@ -1,3 +1,4 @@
+import { ControlDropdownContainerType } from './../../../lib-linear-stepper-ng/src/lib/types/step.type';
 import { StepComponent } from './../../../lib-linear-stepper-ng/src/lib/components/step/step.component';
 import { Component, signal } from '@angular/core';
 
@@ -9,4 +10,10 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('test-application');
+  controlContainer: ControlDropdownContainerType = null;
+
+
+  toggleDropdown(step: ControlDropdownContainerType): void {
+    this.controlContainer = this.controlContainer === step ? null : step;
+  }
 }
